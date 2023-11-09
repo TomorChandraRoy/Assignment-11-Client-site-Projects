b45f3378c44a564fa309d2cc917705582eb99c0f
TomorChandraRoy <tomorchandraroy884@gmail.com>
2023-11-09 08:18:12 +0600

    myjob details page responsive

--- src/components/MyPostedJobPage/MyJobDetails.jsx
+++ src/components/MyPostedJobPage/MyJobDetails.jsx
@@ -43,17 +43,14 @@ const MyJobDetails = ({ singleJob, refetch}) => {
                 data-aos-easing="ease-out-cubic"
                 data-aos-duration="2000" >
 
-                <div className="w-fit h-[405px] bg-green-400 border border-gray-200 rounded-lg shadow-2xl dark:bg-gray-800 dark:border-gray-700 p-5 mb-5">
+                <div className="w-92 md:h-[430px] bg-green-400 border border-gray-200 rounded-lg shadow-2xl dark:bg-gray-800 dark:border-gray-700 p-5 mb-5">
                   
                     <div className="px-5 ">
                         <a href="#">
                             <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-                            <span>{description}</span>
+                            <span >{description}</span>
                         </a>
-                        <span>{Deadline}</span>
-
-
-                        <div className="flex items-center  mt-9">
+                        <div className="flex items-center mt-5 ">
 
                             <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -73,8 +70,8 @@ const MyJobDetails = ({ singleJob, refetch}) => {
                             <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
                             <span className="ml-4">{catogray}</span>
                         </div>
-                        <p>{email}</p>
-                        <div className="flex items-center justify-between pt-9">
+                        <p className="my-2">{email}</p>   <p className="">{Deadline}</p>
+                        <div className="flex items-center md:gap-1 justify-between pt-9">
                             <span className="font-medium text-gray-900 dark:text-white">Max:${maxprice}</span>
                             <span className=" font-medium text-gray-900 dark:text-white">Min:${miniprice}</span>
 
--- src/components/MyPostedJobPage/MyPostJob.jsx
+++ src/components/MyPostedJobPage/MyPostJob.jsx
@@ -18,7 +18,7 @@ const MyPostJob = () => {
         <div>
           
 
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 mt-20" >
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1  gap-5 mt-20" >
                 {
                     data.map(singleJob => <MyJobDetails key={singleJob._id} singleJob={singleJob} refetch={refetch} ></MyJobDetails>)
                 }
