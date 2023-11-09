432ea0115b4257fcfec999c0ebac1933ba3e4fa7
TomorChandraRoy <tomorchandraroy884@gmail.com>
2023-11-09 11:11:50 +0600

    login page complete

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -146,14 +146,3 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
-
-
-
-
-
-
-{/* <a href="#"
-className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-<button  className="btn"> Google</button>
-    
-</a> */}
